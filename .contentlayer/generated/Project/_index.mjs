175d8825ff1c67cf72ab63c033e8a3327135c03d
andy ramaroson <andyramaroson@gmail.com>
2024-11-24 11:02:29 +0100

    (Service) update content service

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -2,7 +2,7 @@
 
 import projects__ballonEauChaudeMdx from './projects__Ballon-eau-chaude.mdx.json' assert { type: 'json' }
 import projects__debouchageDeCanalisationMdx from './projects__Debouchage-de-canalisation.mdx.json' assert { type: 'json' }
+import projects__installationDUnWcMdx from './projects__Installation-d-un-wc.mdx.json' assert { type: 'json' }
 import projects__reparationDUnTuyauMdx from './projects__Reparation-d-un-tuyau.mdx.json' assert { type: 'json' }
-import projects__reparationFuiteMdx from './projects__Reparation-fuite.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__ballonEauChaudeMdx, projects__debouchageDeCanalisationMdx, projects__reparationDUnTuyauMdx, projects__reparationFuiteMdx]
+export const allProjects = [projects__ballonEauChaudeMdx, projects__debouchageDeCanalisationMdx, projects__installationDUnWcMdx, projects__reparationDUnTuyauMdx]
